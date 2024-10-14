d7a732cadfe725321a28111d50869b2ceec9782f
matheuscandido2009@gmail.com <matheuscandido2009@gmail.com>
2024-10-13 23:13:02 -0400

    initial commit

---
+++ scripts/content.js
@@ -0,0 +1,21 @@
+function hideArchivedChats() {
+    let paneSide = document.getElementById("pane-side");
+    
+    if (paneSide) {
+        const buttons = paneSide.getElementsByTagName("button");
+        for (button of buttons) {
+            let attribute = button.getAttribute("aria-label");
+            if (attribute && attribute.startsWith("Archived")) {
+                button.style.display = "none";
+                console.log(button);
+            }
+        }
+    }
+}
+    
+intervalLoop = setInterval(hideArchivedChats, 250);
+
+setTimeout(() => {
+    console.log("Interval Stopped");
+    clearInterval(intervalLoop);
+}, 10000);
